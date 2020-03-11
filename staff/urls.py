54c7310b236445adaefe6163c96a92f969a3c18e
Moshiur Rahman Bhuiyan <moshiurrahman2343@gmail.com>
2020-03-11 18:06:24 +0600

    Add files via upload

--- staff/urls.py
+++ staff/urls.py
@@ -1,41 +1,41 @@
 from django.urls import path, re_path
 from . import views
-
+from django.contrib.auth.decorators import login_required
 
 
 app_name = 'staff'
 
 urlpatterns = [
 
-    path('', views.staff_home, name = "staff_home"),
+    path('', login_required(views.staff_home), name = "staff_home"),
 
 
-    path('view-booking', views.view_booking, name="view_booking"),
+    path('view-booking', login_required(views.view_booking), name="view_booking"),
 
     #Places list and delete
-    path('places/', views.places_list, name="places_list"),
-    path('add-places/', views.add_places, name="add_places"),
+    path('places/',login_required(views.places_list), name="places_list"),
+    path('add-places/', login_required(views.add_places), name="add_places"),
 
     #Hotels list add and delete
-    path('hotels/', views.hotels, name="hotels"),
+    path('hotels/', login_required(views.hotels), name="hotels"),
 
 
     # Room List
-    path('rooms/', views.rooms, name="rooms"),
+    path('rooms/', login_required(views.rooms), name="rooms"),
 
 
 
     # Edit Places
-    re_path('edit-places/(?P<id>\d+)/$',views.edit_places, name="edit_places"),
+    re_path('edit-places/(?P<id>\d+)/$', login_required(views.edit_places), name="edit_places"),
 
 
 
     # Add hotels , Edit Hotel
-    re_path('add-hotel/(?P<place_id>\d+)/$', views.addhotel, name="addhotel"),
-    re_path('edit-hotel/(?P<id>\d+)/$', views.edit_hotel, name="edit_hotel"),
+    re_path('add-hotel/(?P<place_id>\d+)/$', login_required(views.addhotel), name="addhotel"),
+    re_path('edit-hotel/(?P<id>\d+)/$', login_required(views.edit_hotel), name="edit_hotel"),
 
 
     # Add room, Edit Room
-    re_path('add-room/(?P<id>\d+)/$', views.add_room, name="add_room"),
-    re_path('edit-room/(?P<id>\d+)/$', views.edit_room, name = "edit_room"),
+    re_path('add-room/(?P<id>\d+)/$', login_required(views.add_room), name="add_room"),
+    re_path('edit-room/(?P<id>\d+)/$', login_required(views.edit_room), name = "edit_room"),
 ]
